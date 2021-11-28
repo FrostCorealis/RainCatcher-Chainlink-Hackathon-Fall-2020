2f3ae16e0b764c6c123f844de0bf2bddb7b32dae
Frost Corealis <89814258+FrostCorealis@users.noreply.github.com>
2021-11-27 23:59:09 -0500

    Update Weather.sol

--- WeatherWatcher/Weather.sol
+++ WeatherWatcher/Weather.sol
@@ -1,1 +1,132 @@
+// SPDX-License-Identifier: UNLICENSED
 
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import { Base64 } from "./libraries/Base64.sol";
+
+contract Weather is ERC721URIStorage, VRFConsumerBase {
+    using Strings for string;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    mapping(bytes32 => address) internal requestIdToSender;
+    mapping(bytes32 => uint) internal requestIdToTokenId;
+    mapping(uint => uint) internal tokenIdToRandomNumber;
+
+
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    address public vrfCoordinator;
+    address public linkToken;
+
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: darkcyan; font-family: serif; font-size: 39px; }</style><rect width='100%' height='100%' fill='aquamarine' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] weather = [
+        "Rain", 
+        "Snow", 
+        "Sleet", 
+        "Hail", 
+        "Hurricane", 
+        "Tornado", 
+        "Sunshine", 
+        "Partly Cloudy", 
+        "Foggy", 
+        "Drizzle", 
+        "Freezing Rain", 
+        "A Little Rain",
+        "A Little Snow", 
+        "Mostly Cloudy", 
+        "Extemely Sunny", 
+        "Thunderstorm", 
+        "Blizzard", 
+        "Ice Storm", 
+        "Torrential Rain", 
+        "Thundersnow",
+        "Warm Breeze",
+        "Cold Wind",
+        "Fog and Rain"
+    ];
+
+
+      /**
+      network: rinkeby
+      vrf coordinator: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+      link token: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+      key hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+      fee: 0.1 LINK
+
+      network: mumbai
+      vrf coordinator: 0x8C7382F9D8f56b33781fE506E897a4F1e2d17255
+      link token address: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+      key hask: 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4
+      fee: 0.0001 LINK
+      **/
+
+    constructor()
+        ERC721 ("Weather", "WTHR")
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B,
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709)
+         
+    {
+        vrfCoordinator = 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B;
+        linkToken = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 0.1 * 10 ** 18;
+    }
+
+    function predictWeather() public returns (bytes32 requestId){
+    requestId = getRandomNumber();
+    requestIdToSender[requestId] = msg.sender;
+    uint tokenId = _tokenIds.current();
+    requestIdToTokenId[requestId] = tokenId;
+    _tokenIds.increment();
+    }
+
+
+    function getRandomNumber() internal returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - visit the faucet to fill the contract.");
+        return requestRandomness(keyHash, fee);
+    }
+
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        address nftOwner = requestIdToSender[requestId];
+        uint tokenId = requestIdToTokenId[requestId];
+        _safeMint(nftOwner, tokenId);
+        tokenIdToRandomNumber[tokenId] = randomness;
+        string memory prediction = weather[(randomness % weather.length) +1];   //length = 23
+        string memory finalSvg = string(abi.encodePacked(baseSvg, prediction, "</text></svg>"));
+    
+
+        string memory json = Base64.encode(
+                bytes(
+                    string(
+                        abi.encodePacked(
+                    '{"name": "The Weather Today", ', 
+                    '"description": "A truly random weather prediction!", "image": "data:image/svg+xml;base64,', 
+                    Base64.encode(bytes(finalSvg)),
+                            '"}'
+
+                        )
+                    ) 
+                ) 
+            );
+        
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        _safeMint(msg.sender, tokenId);
+    
+        _setTokenURI(tokenId, finalTokenUri);
+  
+        _tokenIds.increment();
+    }
+  
+}
